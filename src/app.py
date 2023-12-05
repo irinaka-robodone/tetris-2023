9f8b7d2ad1c7c3a3688cf3a52547cb2e4c68139e
passive-radio <srccreator@gmail.com>
2023-12-06 00:54:41 +0900

    add pyxel app file

--- src/app.py
+++ src/app.py
@@ -1,5 +1,27 @@
 import pyxel
-from utils import *
+import random
+
+TETROMINOES = {
+    'I': [[1, 1, 1, 1]],
+    'O': [[1, 1],
+          [1, 1]],
+    'T': [[0, 1, 0],
+          [1, 1, 1]],
+    'S': [[0, 1, 1],
+          [1, 1, 0]],
+    'Z': [[1, 1, 0],
+          [0, 1, 1]],
+    'J': [[1, 0, 0],
+          [1, 1, 1]],
+    'L': [[0, 0, 1],
+          [1, 1, 1]]
+}
+
+class Tetrimino:
+    def __init__(self, shape, width):
+        self.shape = shape
+        self.x = width // 2
+        self.y = 0
 
 
 class App:
@@ -130,8 +152,4 @@ class App:
     #                    if 0 <= field_x < self.width and 0<= field_y < self.height:
     #                        self.field[field_y][field_x] = 1  # フィールドにテトリミノを追加
     
-
-
-
-if __name__ == "__main__":
-    App()
+App()
